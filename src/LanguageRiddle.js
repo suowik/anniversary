c04caffe3e69d53100599b47577778b3ddf7a1fc
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-09-25 09:07:03 +0200

    language riddles

--- src/LanguageRiddle.js
+++ src/LanguageRiddle.js
@@ -6,10 +6,10 @@ export default class LanguageRiddle extends CrosswordsRiddle {
         this.state = {
             questions: [
                 {question: "Ile znamy języków miłości?", answer: "", expected: "5", correct: false},
-                {question: "", answer: "", expected: "", correct: false},
-                {question: "", answer: "", expected: "", correct: false},
-                {question: "", answer: "", expected: "", correct: false},
-                {question: "", answer: "", expected: "", correct: false},
+                {question: "fjelltopp sammen", answer: "", expected: "Starorobociański Wierch", correct: false},
+                {question: "Ваш любимый музыкальный инструмент.", answer: "", expected: "skrzypce", correct: false},
+                {question: "Jaki jest język urzędowy Dominikany?", answer: "", expected: "hiszpański", correct: false},
+                {question: "Mój główny język programowania", answer: "", expected: "Java", correct: false},
             ],
             solution: "NIWXIQ",
             title: "Zagadki językowe"
