2b6cde4ef52914f8fb9800ffc1302a48c5197a63
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-09-25 12:56:17 +0200

    movies riddle

--- src/MoviesRiddle.js
+++ src/MoviesRiddle.js
@@ -6,7 +6,7 @@ export default class MoviesRiddle extends CrosswordsRiddle {
         this.state = {
             questions: [
                 {question: "Na jednym ze zdjęć w albumie 'My' są dokładnie 3 postaci - jak nazywa się ta trzecia?", answer: "", expected: "Smerfetka", correct: false},
-                {question: "", answer: "", expected: "", correct: false},
+                {question: "Mój ulubiony bohater z serii Avengers", answer: "", expected: "Iron Man", correct: false},
                 {question: "", answer: "", expected: "", correct: false},
                 {question: "", answer: "", expected: "", correct: false},
                 {question: "", answer: "", expected: "", correct: false},
