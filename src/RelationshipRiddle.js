5608040c18f9232751e58d7ad136ac63eeb156a6
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-10-11 09:14:19 +0200

    dsfas

--- src/RelationshipRiddle.js
+++ src/RelationshipRiddle.js
@@ -6,7 +6,7 @@ export default class RelationshipRiddle extends CrosswordsRiddle {
         this.state = {
             questions: [
                 {question: "Miejsce, w które lubisz być całowana z czułością", answer: "", expected: "czoło", correct: false},
-                {question: "Miejsce, w które lubię być masowany :D", answer: "", expected: "główka", correct: false},
+                {question: "Jaki był największy Zwiazek (duża litera nie jest przypadkiem) w historii Eurazji", answer: "", expected: "radziecki", correct: false},
                 {question: "Gdzie byliśmy na pierwszej kawie?", answer: "", expected: "Jama Michalika", correct: false},
                 {question: "Jaki pierwszy prezent dostałaś ode mnie?", answer: "", expected: "kieliszek", correct: false},
                 {question: "Czego najbardziej nie lubimy?", answer: "", expected: "seler", correct: false}
